b1ba60f2bbf7002a67f8c0c64996e3c5a3841253
devinisaperson <devinisaperson@users.noreply.github.com>
2025-11-01 13:14:50 -0400

    capitalized SequentialCommandTemplate

--- src/main/java/frc/robot/commands/sequentialCommandTemplate.java
+++ src/main/java/frc/robot/commands/sequentialCommandTemplate.java
@@ -11,9 +11,9 @@ import frc.robot.subsystems.flywheel.Flywheel;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class sequentialCommandTemplate extends SequentialCommandGroup {
-  /** Creates a new sequentialCommandTemplate. */
-  public sequentialCommandTemplate(Flywheel flywheel, double velocity) {
+public class SequentialCommandTemplate extends SequentialCommandGroup {
+  /** Creates a new SequentialCommandTemplate. */
+  public SequentialCommandTemplate(Flywheel flywheel, double velocity) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(flywheel.runVelocityCommand(velocity), new WaitCommand(1), flywheel.flywheelStop());
