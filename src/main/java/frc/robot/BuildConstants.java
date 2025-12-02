7486abd08cc4eae930085d451c985ab5565c65ed
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-12-02 18:19:01 -0500

    Small Changes
    Small Changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026TemplateRobotCode";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 60;
-    public static final String GIT_SHA = "b0a86d02b9915105e2839e778b00d4dab2635079";
-    public static final String GIT_DATE = "2025-11-29 01:37:10 EST";
+    public static final int GIT_REVISION = 61;
+    public static final String GIT_SHA = "510aeb5867753ad7f823a02c8957d907e07ed9d0";
+    public static final String GIT_DATE = "2025-11-29 11:29:37 EST";
     public static final String GIT_BRANCH = "heading-lock";
-    public static final String BUILD_DATE = "2025-11-29 11:14:39 EST";
-    public static final long BUILD_UNIX_TIME = 1764432879225L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-11-29 11:36:28 EST";
+    public static final long BUILD_UNIX_TIME = 1764434188214L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
--- src/main/java/frc/robot/commands/drive/HeadingLock.java
+++ src/main/java/frc/robot/commands/drive/HeadingLock.java
@@ -4,22 +4,20 @@
 
 package frc.robot.commands.drive;
 
-import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.drive.holonomic.HolonomicDrive;
 import frc.robot.subsystems.drive.Drive;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class HeadingLock extends Command {
-  /** Creates a new HeadingLock. */
+    /** Creates a new HeadingLock. */
     private Drive drive;
+
     private final Supplier<Translation2d> translationSupplier;
     private DoubleSupplier omegaSupplier;
 
@@ -28,64 +26,48 @@ public class HeadingLock extends Command {
 
     private Rotation2d targetAngle;
 
+    public HeadingLock(Drive drive, Supplier<Translation2d> translationSupplier, DoubleSupplier omegaSupplier) {
+        this.drive = drive;
+        this.translationSupplier = translationSupplier;
+        this.omegaSupplier = omegaSupplier;
 
-  public HeadingLock(Drive drive, Supplier<Translation2d> translationSupplier, DoubleSupplier omegaSupplier) {
-    this.drive = drive;
-    this.translationSupplier = translationSupplier;
-    this.omegaSupplier = omegaSupplier;
+        addRequirements(drive);
+    }
 
+    @Override
+    public void initialize() {
+        targetAngle = drive.getPose().getRotation();
 
-    addRequirements(drive);
-  }
-
-  @Override
-  public void initialize() {
-    targetAngle = drive.getPose().getRotation();
-
-    jStickDrive = new HolonomicDrive(drive,
-     () -> {
+        jStickDrive = new HolonomicDrive(drive, () -> {
             Translation2d driver = translationSupplier.get();
             return ChassisSpeeds.fromFieldRelativeSpeeds(
                     driver.getX(), driver.getY(), omegaSupplier.getAsDouble(), drive.getRotation());
         });
 
-    headingHoldDrive = new HolonomicDrive(drive,
-    () -> {
-      Translation2d driver = translationSupplier.get();
-      return ChassisSpeeds.fromFieldRelativeSpeeds(
-              driver.getX(),
-              driver.getY(),
-              0.0, 
-              targetAngle 
-    );
-  });   
-    
-
-
-  }
-
-  @Override
-  public void execute() {
-    if (omegaSupplier.getAsDouble() > 0.1) {
-      jStickDrive.execute();
-    } else {
-      headingHoldDrive.execute();
+        headingHoldDrive = new HolonomicDrive(drive, () -> {
+            Translation2d driver = translationSupplier.get();
+            return ChassisSpeeds.fromFieldRelativeSpeeds(driver.getX(), driver.getY(), 0.0, targetAngle);
+        });
     }
-    double omega = omegaSupplier.getAsDouble();
-    if (omega >0.1 || omega < -0.1) {
-      targetAngle = drive.getPose().getRotation();
-      jStickDrive.execute();
-    } else {
-      headingHoldDrive.execute();
-    }
-  }
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    @Override
+    public void execute() {
+       
+        double omega = omegaSupplier.getAsDouble();
+        if (omega > 0.1 || omega < -0.1) {
+            targetAngle = drive.getPose().getRotation();
+            jStickDrive.execute();
+        } else {
+            headingHoldDrive.execute();
+        }
+    }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
