db3128837d7e4e763512bc271be95dcdea310518
codertanishrastogi <tanishrastogi2020@gmail.com>
2025-12-22 14:37:43 +0530

    Configured Swerve Modules

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026TemplateRobotCode";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 72;
-    public static final String GIT_SHA = "e43b5f0b4de69b7766f423a1fc2e15704672a2b6";
-    public static final String GIT_DATE = "2025-12-06 11:53:47 EST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-12-06 12:30:45 EST";
-    public static final long BUILD_UNIX_TIME = 1765042245572L;
+    public static final int GIT_REVISION = 73;
+    public static final String GIT_SHA = "a1ce30b4473c624cc71ffbdaff90066e1a562c80";
+    public static final String GIT_DATE = "2025-12-06 13:27:57 EST";
+    public static final String GIT_BRANCH = "TunerConstants";
+    public static final String BUILD_DATE = "2025-12-09 19:28:28 EST";
+    public static final long BUILD_UNIX_TIME = 1765326508144L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
